ab6ca4558971f563c6cb70593b898f5214b7db4b
Josh Sauder <sjoshua270@gmail.com>
2014-10-02 10:00:10 -0400

    Uploading Tester Class and Skeleton Classes

---
+++ src/edu/emu/jzs2255/List.java
@@ -0,0 +1,58 @@
+package edu.emu.jzs2255;
+/**
+ * A class to manage a simple linked list of Strings
+ * @author Josh Sauder (sjoshua270)
+ *
+ */
+public class List {
+	/**
+	 * Constructor method
+	 */
+	public List(){
+		
+	}
+
+	/**
+	 * Inserts a String into the linked list
+	 * in alphabetical order
+	 * @param String to store
+	 * @return True if success
+	 */
+	public boolean insert(String value){
+		
+	}
+	
+	/**
+	 * Finds a String in the linked list
+	 * @param String to search for
+	 * @return True if found
+	 */
+	public boolean find(String word){
+		
+	}
+	
+	/**
+	 * Removes the first instance of a String from the linked list
+	 * @param String to remove
+	 * @return True if removed
+	 */
+	public boolean remove(String word){
+		
+	}
+
+	/**
+	 * Converts the linked list into one String (refer to expected output in the Tester Class)
+	 * @return The list in String form
+	 */
+	public String toString(){
+		
+	}
+	
+	/**
+	 * Checks if there are items in the linked list
+	 * @return True if empty
+	 */
+	public boolean isEmpty(){
+		
+	}
+}
---
+++ src/edu/emu/jzs2255/Node.java
@@ -0,0 +1,48 @@
+package edu.emu.jzs2255;
+/**
+ * Basic node class that holds a String and a Pointer to the next node
+ * @author Josh Sauder (sjoshua270)
+ *
+ */
+public class Node {
+	
+	/**
+	 * Constructor method
+	 * @param Initial String
+	 */
+	public Node(String newValue){
+		
+	}
+	
+	/**
+	 * Returns the value of the Node
+	 * @return String stored in the node
+	 */
+	public String getValue(){
+		
+	}
+	
+	/**
+	 * Sets the value of the Node
+	 * @param New String
+	 */
+	public void setValue(String newValue){
+		
+	}
+	
+	/**
+	 * Returns the next Node in the list
+	 * @return Next Node in the linked list
+	 */
+	public Node getNext(){
+		
+	}
+	
+	/**
+	 * Sets the next Node in the list
+	 * @param New next Node in the linked list
+	 */
+	public void setNext(Node newNext){
+		
+	}
+}
